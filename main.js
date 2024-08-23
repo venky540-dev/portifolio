8d6abeefb4386862aeb48418f3d4cce7803da2f4
venky540-dev <venkateshpannalav@gmail.com>
2024-08-23 10:49:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+    var typed = new Typed('.text', {
+      strings: ['Frontend Developer', 'BackEnd Developer','Web Designer'],
+      typeSpeed: 100,
+ 	backSpeed:100,
+ 	backDeley:1000,
+ 	loop:true
+    });
+    function demo(){
+    var list = document.querySelector('#into');
+    list.style.marginTop="-900px";
+
+    }
+    function remo(){
+    var cross = document.querySelector('#menu-icon');
+    var nav =document.querySelector('#nav');
+    nav.style.marginTop ="0px";
+
+    }
+
